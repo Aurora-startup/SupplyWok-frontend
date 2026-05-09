86e4ac5a5079809e5109234336a43e6a094841d8
Marcelo-alt-lab <marcelocuadrosalt@gmail.com>
2026-05-09 13:17:04 -0500

    feat(sidebar-menu): add sidebar for using with their icons and logo.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SidebarMenuComponent } from './shared/presentation/sidebar-menu/sidebar-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SidebarMenuComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
