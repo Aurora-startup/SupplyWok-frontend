0d7b43c404a067c71ec35c0b2a79e398cb4b49b9
Marcelo-alt-lab <marcelocuadrosalt@gmail.com>
2026-05-09 19:34:54 -0500

    feat(sidebar-menu): add header and language switcher for using with their icons and logo.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { SidebarMenuComponent } from './shared/presentation/sidebar-menu/sidebar-menu.component';
+import { SidebarMenuComponent } from './shared/presentation/components/sidebar-menu/sidebar-menu.component';
+import { LanguageSwitcher } from './shared/presentation/components/language-switcher/language-switcher';
+import { HeaderContent} from './shared/presentation/components/header-content/header-content';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarMenuComponent],
+  imports: [SidebarMenuComponent, LanguageSwitcher, HeaderContent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
